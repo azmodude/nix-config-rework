e3d42ddf6d5e3dfda1f2d3bd51c6d6f9ef43841f
Gordon Schulz <gordon@gordonschulz.de>
2024-10-20 08:52:00 +0200

    feat: Add domain to networking

--- systems/x86_64-linux/vm-minimal/network.nix
+++ systems/x86_64-linux/vm-minimal/network.nix
@@ -1,7 +1,11 @@
 { host, ... }:
+let
+  domain = "hosts.gordonschulz.de";
+in
 {
   networking = {
     hostName = host;
+    inherit domain;
     # useDHCP = false;
     # networkmanager.enable = true;
   };
