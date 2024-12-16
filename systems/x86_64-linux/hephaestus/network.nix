2013d45c3bf32581e97446f15dd51e74b3951db4
Gordon Schulz <gordon@gordonschulz.de>
2024-12-16 14:27:44 +0100

    fix(hephaestus): fix networking and facter interaction

--- systems/x86_64-linux/hephaestus/network.nix
+++ systems/x86_64-linux/hephaestus/network.nix
@@ -1,4 +1,4 @@
-{ host, ... }:
+{ host, lib, ... }:
 let
   domain = "hosts.gordonschulz.de";
 in
@@ -6,7 +6,12 @@ in
   networking = {
     hostName = host;
     inherit domain;
-    # useDHCP = false;
-    # networkmanager.enable = true;
+    useDHCP = lib.mkDefault true;
+  };
+  systemd = {
+    # even if networkd gets enabled, we are not using it on this system.
+    # if this the systemd-networkd-wait-online unit gets enabled, it will fail - because it's not managing anything
+    # therefore don't wait for the wait-online unit
+    network.wait-online.enable = false;
   };
 }
